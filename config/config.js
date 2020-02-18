1a1b45739cedee146b95f6ec088a715c2ad1282f
Miguel Ramos <35831405+MiguelAngelRamos@users.noreply.github.com>
2020-02-18 06:24:48 -0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,4 +2,4 @@ module.exports.SEED = '@este-es@-un-seed-dificil';
 
 // Google
 
-module.exports.CLIENT_ID = '260902693286-qumssgfonbe3aovcs4jefvrfr6hmro9a.apps.googleusercontent.com';
+module.exports.CLIENT_ID = 'xxxxxxxxxxxxxxxxxxxxxx';
